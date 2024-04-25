6835a3c0952d337767734f45cefdf088850667bb
Munhjinn <munhjinmunhjin994@gmail.com>
2024-04-25 14:41:18 +0800

    gemini

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Main from './Main/Main';
+import Sidebar from './Sidebar/Sidebar';
+
+function App() {
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
